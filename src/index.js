b57266c9230a01859b8342da86f1d721c9c2e8ce
Izac <izikki11499@gmail.com>
2022-11-26 11:24:46 -0800

    I THINK I cleaned it up so that the page will default to the home tab when we refresh and first load up the page

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
-//Main Content div in HTMl
-//const content = document.getElementById("content");
-// //Create these elements for our html using JS, and append when page is laoded
-// const image = document.createElement("img"); 
-// const headline = document.createElement("h1");
-// const description = document.createElement("p");
-// //Do this when page first loads up
-// window.onload = (e) => {
-//     //set image src 
-//     image.setAttribute('src', './assets/images/restaurant.png');//
-//     //append elements to #content when page loads up
-//     content.appendChild(image);
-//     content.appendChild(headline);
-//     //set elements text content
-//     headline.textContent = "Welcome to the Restaurant!";
-//     content.appendChild(description);
-//     description.textContent = "This is probably most likeley and definiteley a maybe possibility that this will be the best restauratn you will have ever been to.  Enjoy!";
-// };
 
 import loadHomeTab from "./home";
-//call
-loadHomeTab();
+
+//Default to the home tab when the page is loaded or refreshed
+window.onload = (e) => {
+    loadHomeTab();
+};
 
