afa84d5e287fba4f2886cd0f3ef5f45f3112c398
Izac <izikki11499@gmail.com>
2022-11-26 11:56:05 -0800

    Set up the menu tab, index.js correctly imports the loader from menu.js, now just need to work on the actual content for each tab, and work on an interface for user to switch between tabs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 
 import loadHomeTab from "./home";
 import loadContactTab from "./contact";
+import loadMenuTab from "./menu";
 
 //Default to the home tab when the page is loaded or refreshed
 window.onload = (e) => {
     //loadHomeTab();
     //loadContactTab();
+    loadMenuTab();
 };
 
 //event listeners for tab buttons
--- src/menu.js
+++ src/menu.js
@@ -1,1 +1,18 @@
 //This will be the tab that displays a menu for the user, loads on button/tab click
+const createMenu = () => {
+    const menu = document.createElement("div");
+    menu.classList.add("menu");
+
+    const menuContent = document.createElement("h1");
+    menuContent.textContent = "This is the menu tab";
+    menu.appendChild(menuContent);
+    return menu;
+};
+
+const loadMenuTab = () => {
+    const currentContent = document.getElementById("content");
+    currentContent.textContent = '';
+    currentContent.appendChild(createMenu());
+};
+
+export default loadMenuTab;
