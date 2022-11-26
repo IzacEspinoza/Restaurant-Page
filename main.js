c31c19a5e9db63f565ca2f7585a47f4da816f5e7
Izac <izikki11499@gmail.com>
2022-11-26 11:19:53 -0800

    created module for the home tab content, works nice so far, now need other tabs

--- main.js
+++ main.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n\nconst createHome = () => {\n  const home = document.createElement(\"div\");\n  home.classList.add(\"home\");\n  \n  const restaurantImg = document.createElement(\"img\");\n  restaurantImg.src = \"./assets/images/restaurant.png\";\n  restaurantImg.alt = \"restaurant\";\n  \n  home.appendChild(restaurantImg);\n  home.appendChild(makeHeadLine(\"Welcome to the restaurant!\"));\n  home.appendChild(\n    makeParagraph(\n      \"This will be the best place you ever eat at!  Most likeley at least, we hope :/\"\n    )\n  );\n\n  return home;\n};\nconst makeHeadLine = (text) => {\n  const header = document.createElement(\"h1\");\n  header.textContent = text;\n  return header;\n};\nconst makeParagraph = (text) => {\n  const paragraph = document.createElement(\"p\");\n  paragraph.textContent = text;\n  return paragraph;\n};\nconst loadHomeTab = () => {\n    const currentContent = document.getElementById(\"content\");\n    currentContent.textContent = '';\n    currentContent.appendChild(createHome());\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (loadHomeTab);\n\n\n//# sourceURL=webpack://restaurant-page/./src/home.js?");
+
+/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("//Main Content div in HTMl\nconst content = document.getElementById(\"content\");\n//Create these elements for our html using JS, and append when page is laoded\nconst image = document.createElement(\"img\"); \nconst headline = document.createElement(\"h1\");\nconst description = document.createElement(\"p\");\n//Do this when page first loads up\nwindow.onload = (e) => {\n    //set image src \n    image.setAttribute('src', './assets/images/restaurant.png');\n    //append elements to #content when page loads up\n    content.appendChild(image);\n    content.appendChild(headline);\n    //set elements text content\n    headline.textContent = \"Welcome to the Restaurant!\";\n    content.appendChild(description);\n    description.textContent = \"This is probably most likeley and definiteley a maybe possibility that this will be the best restauratn you will have ever been to.  Enjoy!\";\n};\n\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home */ \"./src/home.js\");\n//Main Content div in HTMl\n//const content = document.getElementById(\"content\");\n// //Create these elements for our html using JS, and append when page is laoded\n// const image = document.createElement(\"img\"); \n// const headline = document.createElement(\"h1\");\n// const description = document.createElement(\"p\");\n// //Do this when page first loads up\n// window.onload = (e) => {\n//     //set image src \n//     image.setAttribute('src', './assets/images/restaurant.png');//\n//     //append elements to #content when page loads up\n//     content.appendChild(image);\n//     content.appendChild(headline);\n//     //set elements text content\n//     headline.textContent = \"Welcome to the Restaurant!\";\n//     content.appendChild(description);\n//     description.textContent = \"This is probably most likeley and definiteley a maybe possibility that this will be the best restauratn you will have ever been to.  Enjoy!\";\n// };\n\n\n//call\n(0,_home__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
