1c8f4180096fb5015d135de1946b87a9481d9bce
Izac <izikki11499@gmail.com>
2022-12-10 00:53:56 -0800

    started prep to build the nav bar for the site, to allow switching between different tabs of pages

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import loadHomeTab from "./home";
 import loadContactTab from "./contact";
 import loadMenuTab from "./menu";
 
+//function to create Nav bar for the site
+const navBar = () =>{
+    //create the nav bar elements: nav, ul-->li's(tabs for different pages)
+    //Append this nav bar and stuff to #content
+}
+
 //Default to the home tab when the page is loaded or refreshed
 window.onload = (e) => {
-    //loadHomeTab();
+    loadHomeTab();
     //loadContactTab();
     //loadMenuTab();
 };
