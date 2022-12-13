1a8cced6f552c04894c855579996198858d908bb
Izac <izikki11499@gmail.com>
2022-12-12 21:20:05 -0800

    Built the websites UI elements, website.js builds all essential nav elements and initializes website

--- src/website.js
+++ src/website.js
@@ -1,6 +1,10 @@
+import loadHomeTab from "./home";
+import loadMenuTab from "./menu";
+import loadContactTab from "./contact";
+
 //all default webcontent will be here, e.g: nav bar, menus, footers
+
 const createHeader = () => {
-    //create the sites header
     const header = document.createElement("header");
     header.classList.add("header");
     
@@ -13,15 +17,76 @@ const createHeader = () => {
 
     return header;
 };
-
 const createNav = () => {
-    //create the main navigation here
+    const nav = document.createElement("nav");
+    nav.classList.add("nav-bar");
+
+    const homeBtn = document.createElement("button");
+    homeBtn.classList.add("tab-button");
+    homeBtn.textContent = "Home";
+    homeBtn.addEventListener("click", (e) => {
+        if(e.target.classList.contains("active")) return;
+        setActiveButton(homeBtn);
+        loadHomeTab();
+    });
+
+    const menuBtn = document.createElement("button");
+    menuBtn.classList.add("tab-button");
+    menuBtn.textContent = "Menu";
+    menuBtn.addEventListener("click", (e) => {
+        if(e.target.classList.contains("active")) return;//pretty much saying if this tab is already active, just return it(mayeb it reloads it? just keep saem active?)
+        setActiveButton(menuBtn);
+        loadMenuTab();
+    });
+
+    const contactBtn = document.createElement("button");
+    contactBtn.classList.add("tab-button");
+    contactBtn.textContent = "Contact";
+    contactBtn.addEventListener("click", (e) => {
+        if(e.target.classList.contains("active")) return;
+        setActiveButton(contactBtn);
+        loadContactTab();
+    });
+
+    nav.appendChild(homeBtn);
+    nav.appendChild(menuBtn);
+    nav.appendChild(contactBtn);
+
+    return nav;
 };
-
-const createFooter = () => {
-    //create the footer fo th site
+const setActiveButton = (button) => {
+    const navButtons = document.querySelectorAll(".tab-button");
+  
+    navButtons.forEach((button) => {
+      if (button !== this) {
+        button.classList.remove("active");
+      }
+    });
+  
+    button.classList.add("active");
 }
+const createFooter = () => {
+    const footer = document.createElement("footer");
+    footer.classList.add("footer");
 
+    const copyright = document.createElement("p");
+    copyright.textContent = `Copyright © ${new Date().getFullYear()} Izac Espinoza`;
+
+    const gitHubLink = documetn.createElement("a");
+    gitHubLink.href = "https://github.com/IzacEspinoza";
+
+    footer.appendChild(copyright);
+    footer.appendChild(gitHubLink);
+}
 const initializeWebsite = () => {
     //load up the site, embed into #content
+    const content = document.getElementById("content");
+
+    content.appendChild(createHeader());
+    content.appendChild(createNav());
+    content.appendChild(createFooter());
+
+    setActiveButton(document.querySelector(".tab-button"));
+    loadHomeTab();
 }
+export default initializeWebsite;
